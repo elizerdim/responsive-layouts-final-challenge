01f829d082232206744bdfd992942ac252cf1291
elizerdim <elizerdim@gmail.com>
2023-06-08 22:03:24 +0300

    Style mobile-first header

---
+++ main.js
@@ -0,0 +1,6 @@
+const navButton = document.querySelector(".nav-toggle");
+const nav = document.querySelector(".nav");
+
+navButton.addEventListener("click", function() {
+    nav.classList.toggle("nav--visible");
+});
