63e9d19f43000d52fb829c4db11c2a8b64f91904
elizerdim <elizerdim@gmail.com>
2023-06-12 20:22:32 +0300

    Add hamburger menu button for small screens

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const navButton = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
+const hamburger = document.querySelectorAll(".hamburger");
 
 navButton.addEventListener("click", function() {
     nav.classList.toggle("nav--visible");
+    navButton.classList.toggle("nav-toggle--active");
+
+    for (let i = 0; i < hamburger.length; i++) {
+        hamburger[i].classList.toggle("hamburger--active");
+    }
+
 });
+
